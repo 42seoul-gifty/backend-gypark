18deb17a27c6fd07a2d643580429d2b7c51d07a0
gghotted <gghotted2@naver.com>
2021-09-06 23:38:48 +0900

    기능: 상품 리스트 필터링 로직 수정
     - multiple choice로 변경
     - 기존 값으로 필터링에서 index로 변경
     - gender가 누락 되어서 추가

--- django_app/gifty/filters.py
+++ django_app/gifty/filters.py
@@ -1,10 +1,24 @@
 from django_filters import (
     FilterSet,
     NumberFilter,
+    ModelMultipleChoiceFilter,
+)
+
+from .models import (
+    AgeCategory,
+    GenderCategory
 )
 
 
 class ProductFilter(FilterSet):
-    price = NumberFilter(field_name='price__value')
-    age_min = NumberFilter(field_name='age__min')
-    age_max = NumberFilter(field_name='age__max')
+    price = NumberFilter(
+        field_name='price__value'
+    )
+    age = ModelMultipleChoiceFilter(
+        field_name='age',
+        queryset=AgeCategory.objects.all()
+    )
+    gender = ModelMultipleChoiceFilter(
+        field_name='gender',
+        queryset=GenderCategory.objects.all()
+    )
